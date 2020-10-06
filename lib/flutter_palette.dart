49c1e31162f259bbe43d104a264026da27e8f2c1
James Alexander <j.alex@email.com>
2020-10-06 02:08:36 -0700

    Rename `palette.dart` to `color_palette.dart`.

--- lib/flutter_palette.dart
+++ lib/flutter_palette.dart
@@ -4,4 +4,4 @@ export 'package:flutter/painting.dart' show Color;
 export 'package:flutter_color_models/flutter_color_models.dart';
 export 'package:palette/palette.dart'
     show ColorSpace, ColorSortingProperty, ColorSortingDirection;
-export 'src/palette.dart';
+export 'src/color_palette.dart';
